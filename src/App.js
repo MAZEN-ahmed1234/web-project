767c412cb470e5ecf170e7d96da7c5c97cfc0091
MAZEN-ahmed1234 <mazenahmed200519@gmail.com>
2026-04-22 14:35:25 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import {Banner} from './components/Banner';
+import { Skills} from './components/Skills';
+import { Projects } from './components/projects';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects/>
+    </div>
+  );
+}
+
+export default App;
