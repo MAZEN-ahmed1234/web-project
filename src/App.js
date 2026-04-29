f3e545ca426fe3f2750ef62b3c97d31e442614c1
MAZEN-ahmed1234 <mazenahmed200519@gmail.com>
2026-04-29 13:48:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import NavBar from './components/NavBar';
 import {Banner} from './components/Banner';
 import { Skills} from './components/Skills';
 import { Projects } from './components/projects';
+import { Contact } from "./components/contact";
+import { Newsletter} from "./components/newsletter";
+import { Footer } from "./components/footer";
 function App() {
   return (
     <div className="App">
@@ -11,8 +14,12 @@ function App() {
       <Banner />
       <Skills />
       <Projects/>
+      <Contact />
+      <Newsletter/>
+        <Footer />
     </div>
   );
 }
 
 export default App;
+
